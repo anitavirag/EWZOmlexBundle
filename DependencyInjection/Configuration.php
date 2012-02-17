0974708f117dec7269a77df3c8b1308505980994
excelwebzone <excelwebzone@gmail.com>
2012-02-17 05:50:18 -0800

    Update Configuration to work with latest Sf2.1

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,6 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('providers')
-                    ->addDefaultsIfNotSet()
                     ->canBeUnset()
                     ->prototype('array')
                     ->children()
